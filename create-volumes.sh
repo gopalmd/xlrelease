0e115c0bb16125537f45925edd8bde5f392ac2cd
Ragu Thangavel <raguyazhin@gmail.com>
2021-11-18 14:04:02 +0530

    initial commit

--- create-volumes.sh
+++ create-volumes.sh
@@ -3,8 +3,18 @@
 mkdir -p XebiaLabs/xl-deploy-server/conf
 mkdir -p XebiaLabs/xl-deploy-server/hotfix/lib
 mkdir -p XebiaLabs/xl-deploy-server/hotfix/plugins
+mkdir -p XebiaLabs/xl-deploy-server/ext
+mkdir -p XebiaLabs/xl-deploy-server/plugins
+mkdir -p XebiaLabs/xl-deploy-server/repository
+mkdir -p XebiaLabs/xl-deploy-server/repository
+mkdir -p XebiaLabs/xl-deploy-server/repository
 
 mkdir -p XebiaLabs/xl-release-server/conf
-mkdir -p XebiaLabs/xl-release-server/hotfix
+mkdir -p XebiaLabs/xl-release-server/hotfix/
+mkdir -p XebiaLabs/xl-release-server/ext
+mkdir -p XebiaLabs/xl-release-server/plugins
+mkdir -p XebiaLabs/xl-release-server/repository
+mkdir -p XebiaLabs/xl-release-server/archive
+mkdir -p XebiaLabs/xl-release-server/archive
 
 sudo chown -R 10001 ~/XebiaLabs/
